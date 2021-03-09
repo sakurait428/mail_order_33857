232af6c00ebc2e7b3d539ae64c6ed73d74eda153
sakurait428 <sakurai_t@hotmail.com>
2021-03-09 14:39:45 +0900

    データベース保存まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :info_brand, :info_material, :info_size, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id)
   end
   
 end
